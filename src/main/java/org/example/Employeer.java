427e4a988b96dc38c8dc6b473735471969348016
ildarneo <ildar_talipov@mail.ru>
2025-01-22 14:20:06 +0300

    изменения в коде

--- src/main/java/org/example/Employeer.java
+++ src/main/java/org/example/Employeer.java
@@ -1,4 +1,38 @@
 package org.example;
 
+
+
+/**
+ * Класс Employeer представляет сотрудника компании.
+ */
 public class Employeer {
+    public String fio;
+    public String position;
+    public String email;
+    public String phone;
+    public int salary;
+    public int age;
+
+    public Employeer(String fio, String position, String email, String phone, int salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("ФИО: " + fio);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public static void main(String[] args) {
+        Employeer employee = new Employeer("Замухрышев Петр Никонорович", "Инженер по безоблачным технологиями", "starover@examp.com","7919691484", 50000, 130);
+        employee.info();
+    }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,19 @@
 package org.example;
 
+/**
+ * массив Person для 5 сотрудников.
+ */
+
 public class Person {
+    public static void main (String [] args) {
+        Person [] perArray = new Person[5];
+        perArray [0] = new Person(" Ivan Ivanov", "Engineer", "ivivan@mailbox.com", "892312312", "30000", "30");
+        perArray [1] = new Person(" Dima Petrov", "Devops", "sdsvan@mail.com", "8923123212", "35000", "31");
+        perArray [2] = new Person(" Ivan Ivanov", "Engineer", "ivivan@mailbox.com", "892312312", "30000", "30");
+        perArray [3] = new Person(" Ivan Ivanov", "Engineer", "ivivan@mailbox.com", "892312312", "30000", "30");
+        perArray [4] = new Person(" Ivan Volkoı", "Engineer", "ivivan@mailbox.com", "892312312", "30000", "30");
+
+    }
 }
+
+//немного не понял задание. только создать массив или что то надо выводить ?
