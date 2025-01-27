3e62affb8c82a08f69e751d07f1d39a1a4f95d0e
ildarneo <59227732+ildarneo@users.noreply.github.com>
2025-01-27 15:03:48 +0300

    Delete src/main/java/Lesson_7 directory

--- src/main/java/Lesson_7/Hello.java
+++
@@ -1,8 +0,0 @@
-package Lesson_7;
-
-import java.util.Arrays;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("args = " + Arrays.toString(args)); }
-}
